7567f863c268ddac4af7e16f5bab2ddc0c291c9c
Hoàng Minh Tài <hoangminhtai2810@gmail.com>
2021-12-19 16:14:41 +0700

    Customize class Service and inherit classes.

--- Service/Service.cs
+++ Service/Service.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Electronics_Store_MS.Service
 {
-    class Service
+    public abstract class Service
     {
+        private string name;
+        private int fee = 500;
+        private bool isAdded = false;
+
+        protected string Name { get => name; set => name = value; }
+        protected int Fee { get => fee; }
+        protected bool IsAdded { get => isAdded; set => isAdded = value; }
+
+        public abstract string AddServiceName();
     }
 }
